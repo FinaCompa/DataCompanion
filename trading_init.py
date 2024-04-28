4341b14151c364a1a7e8f02b1bd4a37bf7cc4a8a
FinaCompa <152924714+FinaCompa@users.noreply.github.com>
2024-04-28 19:24:55 +0200

    Update trading_init.py

--- trading_init.py
+++ trading_init.py
@@ -5,25 +5,11 @@ import pandas as pd
 df = pd.read_csv('datas/BTC_USDT.csv')
 
 register(
-    id='Full-Trade',
-    entry_point='env_perso:Trading',
-    kwargs={
-        'dataframe': deepcopy(df),
-        'window': 31,
-        'scenario': 'test',
-        'len_train_ep': 365,
-        'max_drawdown': 0.85 
-    }
-)
-
-register(
     id='Production',
     entry_point='env_prod:Trading',
     kwargs={
         'dataframe': deepcopy(df),
         'window': 31,
-        'scenario': 'test',
-        'len_train_ep': 365,
         'max_drawdown': 0.85 
     }
 )
